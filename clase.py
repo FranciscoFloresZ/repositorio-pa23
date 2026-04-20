f78ead9d233b25b93a6f02340ed83484390b387e
Francisco Flores <notdemononebv@gmail.com>
2026-04-19 19:08:12 -0600

    Se realizaron todos los get

--- clase.py
+++ clase.py
@@ -5,4 +5,18 @@ class Persona:
         self.__peso = peso
         self.__altura = altura
         self.__ciudad = ciudad
-        
+
+     def get_nombre(self):
+        return self.__nombre
+
+    def get_edad(self):
+        return self.__edad
+
+    def get_peso(self):
+        return self.__peso
+
+    def get_altura(self):
+        return self.__altura
+
+    def get_ciudad(self):
+        return self.__ciudad
