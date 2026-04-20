d3987c3728d288474caffdb2bcb8ffadfed0d9e6
Francisco Flores <notdemononebv@gmail.com>
2026-04-19 19:07:12 -0600

    Clase persona con atributos privados

---
+++ clase.py
@@ -0,0 +1,8 @@
+class Persona:
+    def __init__(self, nombre, edad, peso, altura, ciudad):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__peso = peso
+        self.__altura = altura
+        self.__ciudad = ciudad
+        
